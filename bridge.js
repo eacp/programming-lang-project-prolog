2379f1f0aef15ab7749ef486134e98e4a1bc2466
Eduardo Andrés Castillo Perera <eduardoandrescp@outlook.com>
2020-11-24 06:39:34 -0600

    Add more data to the knowledge base and USA
    Added data to the knowledge base to calculate multiple years and beta support for USA Income Tax as well (only years 2020 and 2019)

--- bridge.js
+++ bridge.js
@@ -15,27 +15,40 @@ session.consult("knowledge-base.pl", {
 // Functions to evaluate data
 
 // Function to construct the prolog command
-const makeQuery = (income, period) => `payment(${income},${period}, Whithold, Deposit).`;
+const makeQuery = (income, period, year) => `payment(${income},${period}, Whithold, Deposit,${year}).`;
 
 
 // Get the fields to be used later
 const incomeField = document.getElementById('income');
 const periodField = document.getElementById('frequency');
+const yearField   = document.getElementById('year');
+
+
 const resultsArea = document.getElementById('res');
+
+const queryFailMessage = "Error: There is no information available in the Knowledge Base to answer this quey. This is likely due to missing US Income Tax data for previous years";
 
 // Function to grab the data and pass it to prolog
 const calc = () => {
     // Get the data
     const income = incomeField.value;
     const period = periodField.value;
+    const year = yearField.value;
+
     // Make the query
-    const q = makeQuery(income,period);
+    const q = makeQuery(income,period, year);
     // Execute the query
     console.log('Executing: ',q);
     session.query(q);
     // Tau Prolog requires this to be asyncronous
     session.answer((answer) => {
         console.log(answer);
+        if(!answer) {
+            // Query failed
+            alert(queryFailMessage);
+            return;
+        }
+
         // Get the data from the answer
         const deposit = answer.links.Deposit.value;
         const tax = answer.links.Whithold.value;
